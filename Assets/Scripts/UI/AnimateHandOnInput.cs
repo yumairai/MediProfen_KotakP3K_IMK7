792789355b87dfb1a7b0f7ed3572b070e9d3ee79
Orlando Bloem Sutono <>
2026-05-07 10:51:07 +0700

    feat: add grab logic

---
+++ Assets/Scripts/UI/AnimateHandOnInput.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+
+    public InputActionProperty triggerValue;
+    public InputActionProperty gripValue;
+    public Animator handAnimator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float trigger = triggerValue.action.ReadValue<float>();
+        float grip = gripValue.action.ReadValue<float>();
+
+        if (handAnimator != null)
+        {
+            handAnimator.SetFloat("Trigger", trigger);
+            handAnimator.SetFloat("Grip", grip);
+        }
+    }
+}
